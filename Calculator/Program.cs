ea774146d7de4093237f46d0e72835944b2a61ed
RafitaSoler <rafaelsolerfollana@gmail.com>
2026-06-12 13:10:18 +0200

    Added extra operations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,56 +1,42 @@
-﻿using System.Text.RegularExpressions;
-
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
+﻿using CalculatorLibrary;
 
 class Program
 {
     static void Main(string[] args)
     {
         bool endApp = false;
-        // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
+        Calculator calculator = new Calculator();
         while (!endApp)
         {
-            // Declare variables and set to empty.
-            // Use Nullable types (with ?) to match type of System.Console.ReadLine
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\t0 - Addition");
+            Console.WriteLine("\t1 - Substraction");
+            Console.WriteLine("\t2 - Multiplication");
+            Console.WriteLine("\t3 - Division");
+            Console.WriteLine("\t4 - Exponentiation");
+            Console.WriteLine("\t5 - SquareRoot");
+            Console.WriteLine("\t6 - 10x");
+            Console.WriteLine("\t7 - Sin");
+            Console.WriteLine("\t8 - Cos");
+            Console.WriteLine("\t9 - Tan");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+            int cleanOp = -1;
+            while (!int.TryParse(op, out cleanOp) || cleanOp < 0 || cleanOp > 9)
+            {
+                Console.Write("This is not valid input. Please enter a numeric value between 0 and 9: ");
+                op = Console.ReadLine();
+            }
+
             string? numInput1 = "";
             string? numInput2 = "";
             double result = 0;
 
-            // Ask the user to type the first number.
             Console.Write("Type a number, and then press Enter: ");
             numInput1 = Console.ReadLine();
 
@@ -61,56 +47,46 @@ class Program
                 numInput1 = Console.ReadLine();
             }
 
-            // Ask the user to type the second number.
-            Console.Write("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
-
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
+            double cleanNum2 = double.NaN;
+            // Operators 0 to 4 are binary so they need a second number. 5 to 9 are unary
+            if (cleanOp < 5)
             {
-                Console.Write("This is not valid input. Please enter a numeric value: ");
+                Console.Write("Type another number, and then press Enter: ");
                 numInput2 = Console.ReadLine();
+
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    numInput2 = Console.ReadLine();
+                }
             }
 
-            // Ask the user to choose an operator.
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string? op = Console.ReadLine();
-
-            // Validate input is not null, and matches the pattern
-            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            try
             {
-                Console.WriteLine("Error: Unrecognized input.");
+                result = calculator.DoOperation(cleanNum1, cleanNum2, cleanOp);
+
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
             }
-            else
+            catch (Exception e)
             {
-                try
-                {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
-                    {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                    }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-                }
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
             }
             Console.WriteLine("------------------------\n");
 
-            // Wait for the user to respond before closing.
             Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
             if (Console.ReadLine() == "n") endApp = true;
 
-            Console.WriteLine("\n"); // Friendly linespacing.
+            Console.WriteLine("\n");
         }
+
+        calculator.Finish();
         return;
     }
 }
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,75 @@
+﻿namespace CalculatorLibrary
+{
+    public enum Operator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+        Exponentiation,
+        SquareRoot,
+        x10,
+        Sin,
+        Cos,
+        Tan
+    }
+
+    public record struct Operation(Operator @operator, double number1, double number2, double result);
+
+    public class Calculator
+    {
+        List<Operation> previousOperations = new();
+        Logger logger = new();
+
+        public double DoOperation(double num1, double num2, int op)
+        {
+            double result = double.NaN;
+            Operator @operator = (Operator)op;
+            switch (@operator)
+            {
+                case Operator.Addition:
+                    result = num1 + num2;
+                    break;
+                case Operator.Substraction:
+                    result = num1 - num2;
+                    break;
+                case Operator.Multiplication:
+                    result = num1 * num2;
+                    break;
+                case Operator.Division:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                case Operator.Exponentiation:
+                    result = Math.Pow(num1, num2);
+                    break;
+                case Operator.SquareRoot:
+                    result = Math.Sqrt(num1);
+                    break;
+                case Operator.x10:
+                    result = num1 * 10;
+                    break;
+                case Operator.Sin:
+                    result = Math.Sin(num1);
+                    break;
+                case Operator.Cos:
+                    result = Math.Cos(num1);
+                    break;
+                case Operator.Tan:
+                    result = Math.Tan(num1);
+                    break;
+                default:
+                    break;
+            }
+            previousOperations.Add(new Operation(@operator, num1, num2, result));
+            return result;
+        }
+
+        public void Finish()
+        {
+            logger.Log(previousOperations);
+        }
+    }
+}
